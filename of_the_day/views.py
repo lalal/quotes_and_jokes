fcf742ce2061f4b7bc12d5fd62bfdd7e500844b2
Alok Lal <alok.n.lal@gmail.com>
2022-01-19 00:43:42 -0500

    Updated with api_keys and modified view

--- of_the_day/views.py
+++ of_the_day/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import requests
+from api_keys import api_keys
 
 def index(request):
     return HttpResponse("Joke of the Day")
 
 def quote_of_the_day(request):
+    print(api_keys.dad_jokes_headers)
     return HttpResponse("Quote of the Day")
 
 def relig_quote_of_the_day(request):
